1f0525def58f27daaaba2e3412c1c08ee629153a
Andrea Haupt <hauptaj@gmail.com>
2017-02-15 10:15:29 -0500

    added animation to spotlights on index.html

--- js/main.js
+++ js/main.js
@@ -3,14 +3,3 @@ $(document).ready(function() {
     $(this).attr('src', "assets/occupiedSeat.png");
   });
 });
-
-$(".leftImg").rotate({bind:{
-  click: function(){
-    $(this).rotate({
-      angle: 0,
-      center: ["50%", "100%"],
-      animateTo:180
-      })
-    }
-  }
-});
