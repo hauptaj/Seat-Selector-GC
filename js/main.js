43fab7308ed714cf7eeb54a6a680e0d219a312c0
Spencer White <swhite215@live.com>
2017-02-15 11:20:35 -0500

    Changed Phone Id

--- js/main.js
+++ js/main.js
@@ -8,17 +8,28 @@
 //   title="Popover title" data-content="Do you wish to reserve this Seat?">choose</a> </p>
 //   <hr>
 
-$(document).ready(function() {
-  $("img.seat").on('click', function() {
-    $(this).attr('src', "assets/selectSeat.png");
-    console.log($(this).attr('id'));
-  });
+// $(document).ready(function() {
+//   $("img.seat").on('click', function() {
+//     $(this).attr('src', "assets/selectSeat.png");
+//     console.log($(this).attr('id'));
+//     //form class changes to visible
+//   });
+//
+//
+//
+//
+//   $("img.seat").on('click', function() {
+//     $('[data-toggle="tooltip"]').tooltip();
+//   });
+//
+// });
 
+function userInfo(firstName, lastName, email, phone) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.email = email;
+  this.phone = phone;
+}
 
-
-
-  $("img.seat").on('click', function() {
-    $('[data-toggle="tooltip"]').tooltip();
-  });
-  
-});
+var a1 = new userInfo("Spencer", "White", "swhite215@live.com", 3133483830);
+console.log(a1);
