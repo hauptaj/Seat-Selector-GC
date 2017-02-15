61e5ce549e4f84b1c222dd2bb58c4e1d1f66840b
Andrea Haupt <hauptaj@gmail.com>
2017-02-14 23:36:10 -0500

    added a class to seat images so images on landing page don't change when clicked on

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
-  $("img").on('click', function() {
+  $("img.seat").on('click', function() {
     $(this).attr('src', "assets/occupiedSeat.png");
   });
+});
+
+$(".leftImg").rotate({bind:{
+  click: function(){
+    $(this).rotate({
+      angle: 0,
+      center: ["50%", "100%"],
+      animateTo:180
+      })
+    }
+  }
 });
