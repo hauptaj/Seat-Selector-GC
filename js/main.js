36f0ef52bee871a1b47e8af7be64024c10279064
Spencer White <swhite215@live.com>
2017-02-15 14:11:56 -0500

    Hello

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
+
+    var seatArray = [];
+
     $("img.seat").on('click', function() {
         $(this).attr('src', "assets/selectSeat.png");
-        console.log($(this).attr('id'));
         //form class changes to visible
     });
 
@@ -14,25 +16,22 @@ $(document).ready(function() {
         $('[data-toggle="tooltip"]').tooltip();
     });
 
-    $('button#loadButton').on('click', function() {
-        var practice = new userInfo($("#firstName").val(),
-            $("#lastName").val(), $("#email").val(), $("#phone").val());
+    $('button #loadButton').on('click', function() {
+        var practice = new userInfo($("#firstName").val(), $("#lastName").val(), $("#email").val(), $("#phone").val());
         console.log(practice);
         seatArray.push(practice);
         console.log(seatArray);
-        // console.log($("#firstName").val());
-        // console.log($("#lastName").val());
-        // console.log($("#email").val());
-        // console.log($("#phone").val());
+        console.log($("#firstName").val());
+        console.log($("#lastName").val());
+        console.log($("#email").val());
+        console.log($("#phone").val());
     });
+
+    function userInfo(firstName, lastName, email, phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+    }
+
 });
-
-
-var seatArray = [];
-
-function userInfo(firstName, lastName, email, phone) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.email = email;
-    this.phone = phone;
-}
