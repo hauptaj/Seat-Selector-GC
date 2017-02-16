2128d7f0c23b19357ee5b1bf1265f835de8de415
Jennifer Bonapace <jennifer.bonapace@gmail.com>
2017-02-16 13:26:40 -0500

    j

--- js/main.js
+++ js/main.js
@@ -9,57 +9,57 @@ $(document).ready(function() {
 
     //form always slidesDown when seat is selected.
     $('img.seat').on('click', function() {
-      //make sure form only drops when seat is open or selected.
-      if ($(this).attr('src') === 'assets/openSeat.png' || $(this).attr('src') === 'assets/selectSeat.png') {
-      $('form').slideDown('slow');
-      $('div.popup').slideDown('very slow');
-    }
-  });
+        //make sure form only drops when seat is open or selected.
+        if ($(this).attr('src') === 'assets/openSeat.png' || $(this).attr('src') === 'assets/selectSeat.png') {
+            $('form').slideDown('slow');
+            $('div.popup').slideDown('very slow');
+        }
+    });
 
     // function to alter seat images and input seat ID.
     $("img.seat").on('click', function() {
-      //change open seat to selected seat
+        //change open seat to selected seat
         if ($(this).attr('src') === 'assets/openSeat.png') {
-          $(this).attr('src', 'assets/selectSeat.png');
-          var seatId = $(this).attr("id");
-          $("#seatPlace").val(seatId);
-      //change selected seats to open seats.
+            $(this).attr('src', 'assets/selectSeat.png');
+            var seatId = $(this).attr("id");
+            $("#seatPlace").val(seatId);
+            //change selected seats to open seats.
         } else if ($(this).attr('src') === 'assets/selectSeat.png') {
-          $(this).attr('src', 'assets/openSeat.png');
-          $("#seatPlace").val('');
-      //display info for occupied seats.
+            $(this).attr('src', 'assets/openSeat.png');
+            $("#seatPlace").val('');
+            //display info for occupied seats.
         } else if ($(this).attr('src') === 'assets/occupiedSeat') {
-          for (var i = 0; i < seatArray.length; i++) {
-            if ($(this).attr('id') === seatArray[i].seatPosition) {
+            for (var i = 0; i < seatArray.length; i++) {
+                if ($(this).attr('id') === seatArray[i].seatPosition) {
 
+                }
             }
-          }
         }
     });
 
     //function for tooltip hover Bootstrap*
-    $( function() {
-      $( "img.seat" ).tooltip();
+    $(function() {
+        $("img.seat").tooltip();
     });
 
     //submit button to store form information in array.
     $('button#loadButton').on('click', function() {
 
-        var practice = new userInfo($("#seatPlace").val(), $("#firstName").val(),$("#lastName").val(), $("#email").val(), $("#phone").val());
+        var practice = new userInfo($("#seatPlace").val(), $("#firstName").val(), $("#lastName").val(), $("#email").val(), $("#phone").val());
         seatArray.push(practice);
 
-    //function to change selected seats to occupied seats after submit confirmation.
+        //function to change selected seats to occupied seats after submit confirmation.
         $('img.seat').each(function() {
-          for (var i = 0; i < seatArray.length; i++) {
-            if ($(this).attr('id') === seatArray[i].seatPosition) {
-              $(this).attr('src', "assets/occupiedSeat.png");
-              //method to adjust tooltip text to appropriate reserved data.
-              $(this).attr('title', "Reserved for: " + seatArray[i].firstName + " " + seatArray[i].lastName).tooltip('fixTitle');
+            for (var i = 0; i < seatArray.length; i++) {
+                if ($(this).attr('id') === seatArray[i].seatPosition) {
+                    $(this).attr('src', "assets/occupiedSeat.png");
+                    //method to adjust tooltip text to appropriate reserved data.
+                    $(this).attr('title', "Reserved for: " + seatArray[i].firstName + " " + seatArray[i].lastName).tooltip('fixTitle');
+                }
             }
-          }
-      });
+        });
 
-      //clear all previously entered values to reset form.
+        //clear all previously entered values to reset form.
         $('#seatPlace').val('');
         $('#firstName').val('');
         $('#lastName').val('');
@@ -70,8 +70,8 @@ $(document).ready(function() {
 
     //form slides up after submit.
     $('button#loadButton').on('click', function() {
-      $('form').slideUp('slow');
-      $('div.popup').slideUp('slow');
+        $('form').slideUp('slow');
+        $('div.popup').slideUp('slow');
     })
 
     //constructor function to create object of person's information.
